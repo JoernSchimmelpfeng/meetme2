3c40c750cf9d2fbae97c133976f96852746882e7
Joern <joern.schimmelpfeng@gmail.com>
2018-09-17 19:53:13 +0200

    added comments and minor changes

--- meetme-server/src/main/java/de/meetme/data/Person.java
+++ meetme-server/src/main/java/de/meetme/data/Person.java
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.Entity;
 
-@Entity
+/**
+ * Plain data object which can be stored in the database or transfered as JSON.
+ *
+ * Never add additional logical to this class. By intent it only contains data.
+ */
+@Entity // Declare to dropwizard that this is a persisted object. A table will be created automatically.
 public class Person extends PersistentObject {
     // create table person ( id int, name varchar(256), firstname varchar(256), email varchar(256))
 
@@ -24,17 +29,17 @@ public class Person extends PersistentObject {
         this.email = email;
     }
 
-    @JsonProperty
+    @JsonProperty // if the object is streamed as JSON id will be included
     public String getFirstName() {
         return firstName;
     }
 
-    @JsonProperty
+    @JsonProperty // if the object is streamed as JSON id will be included
     public String getName() {
         return name;
     }
 
-    @JsonProperty
+    @JsonProperty // if the object is streamed as JSON id will be included
     public String getEmail() {
         return email;
     }
